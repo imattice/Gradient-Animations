1ea112ced72f74868f32b609098bdd282c7c138b
Ike Mattice <ikemattice@icloud.com>
2018-06-03 19:35:22 -0700

    Add the gradient animation

--- SlideToReveal/AnimatedMaskLabel.swift
+++ SlideToReveal/AnimatedMaskLabel.swift
@@ -30,7 +30,19 @@ class AnimatedMaskLabel: UIView {
     let gradientLayer = CAGradientLayer()
     
     // Configure the gradient here
-    
+	gradientLayer.startPoint	= CGPoint(x: 0.0, y: 0.5)
+	gradientLayer.endPoint		= CGPoint(x: 1.0, y: 0.5)
+
+	gradientLayer.colors 	= [
+		UIColor.black.cgColor,
+		UIColor.white.cgColor,
+		UIColor.black.cgColor
+	]
+	gradientLayer.locations = [
+		0.25,
+		0.5,
+		0.75
+	]
     return gradientLayer
   }()
   
@@ -42,11 +54,26 @@ class AnimatedMaskLabel: UIView {
   
   override func layoutSubviews() {
     layer.borderColor = UIColor.green.cgColor
+	gradientLayer.frame = CGRect(x: 		-bounds.size.width,
+								 y: 		bounds.origin.y,
+								 width: 	3 * bounds.size.width,
+								 height: 	bounds.size.height)
   }
   
   override func didMoveToWindow() {
     super.didMoveToWindow()
-    
+
+	layer.addSublayer(gradientLayer)
+
+	let gradientAnimation = CABasicAnimation(keyPath: "locations")
+		gradientAnimation.fromValue 	= [0.0, 0.0, 0.25]
+		gradientAnimation.toValue		= [0.75, 1.0, 1.0]
+		gradientAnimation.duration		= 3.0
+		gradientAnimation.repeatCount 	= Float.infinity
+
+	gradientLayer.add(gradientAnimation, forKey: nil)
   }
+
+
   
 }
